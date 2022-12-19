0d3acacaa76d18b31fe70d4a4ab4f913f4f70520
Dmitriy <dmitriev-job@mail.ru>
2022-12-19 18:27:13 +0400

    HomeTask2 wiht methods

--- Home_task_2/Program.cs
+++ Home_task_2/Program.cs
@@ -1,17 +1,24 @@
 ﻿// Задача 10: Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
-
 /*
+int SecondNumber(int num)
+{
+    int findSecond = Math.Abs(num / 10) % 10;
+    return findSecond;
+}
 Console.WriteLine("Input number bitween 100 and 999:");
 int num = Convert.ToInt32(Console.ReadLine());
 
-int findSecond = Math.Abs(num / 10) % 10;
-Console.WriteLine("second number is " + findSecond);
+Console.WriteLine("second number is " + SecondNumber(num));
 */
 
 
 // Задача 13: Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
-
 /*
+int Third(int num)
+{
+    int third = Math.Abs(num / 100) % 10;
+    return third;
+}
 Console.WriteLine("Input number :");
 int num = Convert.ToInt32(Console.ReadLine());
 
@@ -21,36 +28,41 @@ if (num < 100 && num > -100)
 }
 else
 {
-    int third = Math.Abs(num / 100) % 10;
-    Console.WriteLine("third number is " + third);
+    int thirdNum = Third(num);
+    Console.WriteLine("third number is " + thirdNum);
 }
 */
 
 
 // Задача 15: Напишите программу, которая принимает на вход цифру, обозначающую день недели, и проверяет, является ли этот день выходным.
-
 /*
+bool WeekEnd(int dayNum)
+{
+    if (dayNum >= 6)
+        return true;
+    else
+    {
+        return false;
+    }
+}
+
 link1:
-Console.WriteLine("Input number dey of week:");
+Console.WriteLine("Input number day of week:");
 int dayNum = Convert.ToInt32(Console.ReadLine());
 
-if (dayNum < 1)
-{
-    Console.WriteLine("Wrong day number, try again");
-    goto link1;
-}
-else if (dayNum > 7)
+if (dayNum < 1 || dayNum > 7)
 {
     Console.WriteLine("Wrong day number, try again");
     goto link1;
 }
 else
 {
-    if (dayNum >= 6)
+    if (WeekEnd(dayNum))
         Console.WriteLine("It's weekend day");
     else
     {
-        Console.WriteLine("It's not weekend day");
+        Console.WriteLine("It's NOT weekend day");
     }
 }
+
 */
